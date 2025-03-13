c830111d4488d2b6fcabc91a71d140cdf32f3ef1
mccaslinlovingood779 <mccaslinlovingood779@gmail.com>
2025-03-13 11:31:08 +0000

    Add 7.go

---
+++ src/7.go
@@ -0,0 +1,15 @@
+  package main
+
+import "fmt"
+
+func main() {
+	// Declare variables
+	var x int = 5
+	var y float32 = 10.5
+	var z string = "hello world"
+
+	// Print the values of the variables
+	fmt.Println("The value of x is", x)
+	fmt.Println("The value of y is", y)
+	fmt.Println("The value of z is", z)
+}
